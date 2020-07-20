c1685d94aa16e6eef7602ecc49ee1ab9374c37b2
Esteban Rao <estebanrao@gmail.com>
2020-07-20 14:18:45 -0300

    Adds word post

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>My Blog</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink to={node.fields.slug}>
